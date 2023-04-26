c5caaa8d06548baeadca11ddc051685d8879d97b
zarifa-mammadli <108187164+zarifa-mammadli@users.noreply.github.com>
2023-04-27 00:29:01 +0400

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,11 @@ import pickle
 
 df = pd.read_table('modified_aze_link_sentences.tsv')
 
-# Get the length of the DataFrame and divide it by 300
-# to get the total number of iterations
-num_iterations = len(df) // 300
+num_iterations = len(df) // 200
 
-for i in range(num_iterations):
-    start_index = i * 10
-    end_index = (i + 1) * 10
+for i in range(num_iterations+1):
+    start_index = i * 200
+    end_index = (i + 1) * 200
     lst = df.iloc[:, 0].tolist()[start_index:end_index]
     
     list_json = []
@@ -33,7 +31,7 @@ for i in range(num_iterations):
         lst.append('ERROR')  
     
     
-    with open(f"./data/{start_index}_{end_index}_translation_data_test.pickle", "ab") as fp:
+    with open(f"./data/{start_index}_{end_index}_translation_data.pickle", "ab") as fp:
         pickle.dump(list_json, fp)
         fp.close()
 
