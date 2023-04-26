dcc0626e221e81c5980b62b5a536c336c92e1613
zarifa-mammadli <108187164+zarifa-mammadli@users.noreply.github.com>
2023-04-27 00:18:28 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 from bs4 import BeautifulSoup
 import requests
 import json
@@ -6,16 +7,18 @@ import pickle
 
 df = pd.read_table('modified_aze_link_sentences.tsv')
 
+# Get the length of the DataFrame and divide it by 300
+# to get the total number of iterations
 num_iterations = len(df) // 300
 
 for i in range(num_iterations):
-    start_index = i * 200
-    end_index = (i + 1) * 200
+    start_index = i * 300
+    end_index = (i + 1) * 300
     lst = df.iloc[:, 0].tolist()[start_index:end_index]
     
     list_json = []
-    
-    for url in lst:
+    try:
+      for url in lst:
         vstr = requests.get(url).content
         soup = BeautifulSoup(vstr, features="html.parser")
     
@@ -26,6 +29,9 @@ for i in range(num_iterations):
             new_data = str(data).replace("'und'", '"und"')
             new_data = json.loads(new_data)
             list_json.append(new_data)
+    except:
+        lst.append('ERROR')  
+    
     
     with open(f"./data/{start_index}_{end_index}_translation_data.pickle", "ab") as fp:
         pickle.dump(list_json, fp)
